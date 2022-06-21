716b2a8e805c3337bd04fa0ec0d4289b79f2ddfe
unknown <stmarisnwokike@gmail.com git config --global user.name smarist>
2022-06-21 16:11:44 +0100

    finished up the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from "../images/logo.svg"
 import menu from "../images/icon-menu.svg"
 import cart from "../images/icon-cart.svg"
 import avatar from "../images/image-avatar.png"
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    function handleClick(){
+        console.log("done")
+        setIsMenuOpen(!isMenuOpen)
+    }
+    console.log(isMenuOpen)
   return (
-    <header>
-        <nav>
-            <ul>
-                <li><img src={menu} alt="menu"/></li>
-                <li><img src={logo} alt="logo"/></li>
+    <header className='f-row'>
+        <nav className='f-row'>
+            <ul className='f-row row-r menu-ul'>
+                <li className='l-hidden' onClick={() => handleClick()}><img src={menu} alt="menu"/></li>
+                <ul className= {isMenuOpen? "f-col" : 'f-row s-hidden nav-menu'}>
+                    <li>Collections</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+                <li className='logo-menu'  ><img src={logo} alt="logo" id='logo'/></li>
             </ul>
-            <ul>
-                <li><img src={cart} alt="cart"/></li>
-                <li><img src={avatar} alt="avatar"/></li>
+            <ul className='f-row cart-avatar'>
+                <li><img src={cart} alt="cart" /></li>
+                <li><img src={avatar} alt="avatar" id='avatar'/></li>
             </ul>
         </nav>
     </header>
